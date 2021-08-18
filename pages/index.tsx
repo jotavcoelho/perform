5d0b06a0506b8a902ad8ff0b37cc9414b59afa0f
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2021-08-18 19:54:59 -0300

    completed the search feature, which will be what we'll learn performance upon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,34 @@
+import { FormEvent, useState } from "react"
+
 export default function Home() {
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
+
+  async function handleSearch(event: FormEvent) {
+    event.preventDefault();
+
+    if (!search.trim()) {
+      return;
+    }
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`);
+    const data = await response.json();
+
+
+  }
+
   return (
-    <h1>hello modafoca</h1>
+    <div>
+      <h1>Search</h1>
+
+      <form onSubmit={handleSearch}>
+        <input 
+          type="text" 
+          value={search} 
+          onChange={e => setSearch(e.target.value)}
+        />
+        <button type="submit">Go</button>
+      </form>
+    </div>  
   )
 }
