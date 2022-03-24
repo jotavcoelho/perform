0334b84047165e4ed29152dee393f1a28675a65c
jotavcoelho <jotavcoelhodev@gmail.com>
2022-03-24 15:56:55 -0300

    using useCallback

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -8,9 +8,10 @@ interface SearchResultsProps {
     price: number;
     title: number;
   }>
+  onAddToWishList: (id: number) => void;
 }
 
-export function SearchResults ({results}: SearchResultsProps) {
+export function SearchResults ({ results, onAddToWishList}: SearchResultsProps) {
   const totalPrice = useMemo(() => {
     return results.reduce((total, product) => {
       return total + product.price;
@@ -22,7 +23,11 @@ export function SearchResults ({results}: SearchResultsProps) {
       <h2>{totalPrice}</h2>
       {results.map(product => {
         return (
-          <ProductItem product={product} key={product.id}/>
+          <ProductItem 
+            key={product.id}
+            product={product} 
+            onAddToWishList={onAddToWishList}
+          />
         );
       })}
     </div>
