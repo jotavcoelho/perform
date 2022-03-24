86b3e22f46204c72a87d569bdd509cce46164ab5
jotavcoelho <jotavcoelhodev@gmail.com>
2022-03-24 11:43:58 -0300

    basic application created in order to test the performance stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,38 @@
 import type { NextPage } from 'next'
+import { FormEvent, useState } from 'react'
+import { SearchResults } from '../components/SearchResults';
 
 const Home: NextPage = () => {
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
+
+  async function handleSearch(event: FormEvent) {
+    event.preventDefault();
+
+    if (!search.trim()) {
+      return;
+    }
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`)
+    const data = await response.json();
+
+    setResults(data);
+  }
+
   return (
-    <h1>Hello modafoca</h1>
+    <div>
+      <h1>Search</h1>
+
+      <form onSubmit={handleSearch}>
+        <input 
+          type="text" 
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+        />
+      </form>
+      
+      <SearchResults results={results}/>
+    </div>
   )
 }
 
