d2bea9cceee326003a5054477a11482260796691
jotavcoelho <jotavcoelhodev@gmail.com>
2022-03-24 14:44:33 -0300

    using useMemo

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from "react";
+
 import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
@@ -9,8 +11,15 @@ interface SearchResultsProps {
 }
 
 export function SearchResults ({results}: SearchResultsProps) {
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price;
+    }, 0)
+  }, [results])
+
   return (
     <div>
+      <h2>{totalPrice}</h2>
       {results.map(product => {
         return (
           <ProductItem product={product} key={product.id}/>
