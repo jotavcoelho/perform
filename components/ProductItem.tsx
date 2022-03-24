31dc491ec1d26d46ae5fdcc7e1b29730736ce982
jotavcoelho <jotavcoelhodev@gmail.com>
2022-03-24 18:08:37 -0300

    using dynamic imports

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,4 +1,15 @@
-import { memo } from 'react';
+import { memo, useState } from 'react';
+
+import dynamic from 'next/dynamic';
+
+import { AddProductToWishlistProps } from './AddProductToWishlist'
+// import { AddProductToWishlist } from './AddProductToWishlist';
+
+const AddProductToWishlist = dynamic<AddProductToWishlistProps>(() => {
+  return import ('./AddProductToWishlist').then(mod => mod.AddProductToWishlist)
+}, {
+  loading: () => <span>Carregando...</span>
+})
 
 interface ProductItemProps {
   product: {
@@ -11,10 +22,26 @@ interface ProductItemProps {
 }
 
 function ProductItemComponent({ product, onAddToWishList }: ProductItemProps) {
+  const [willAddToWishList, setWillAddToWishList] = useState(false);
+
+  // we can also import functions dynamically
+  // async function showFormattedDate() {
+  //   const { format } = await import('date-fns')
+
+  //   format()...
+  // }
+
   return (
     <div>
       {product.title} - <strong>{product.formattedPrice}</strong>
-      <button onClick={() => onAddToWishList(product.id)}>Add to wishlist</button>
+      <button onClick={() => {setWillAddToWishList(true)}}>Adicionar aos favoritos</button>
+
+      {willAddToWishList && (
+        <AddProductToWishlist
+          onAddToWishList={() => onAddToWishList(product.id)}
+          onRequestClose={() => setWillAddToWishList(false)}
+        />
+      )}
     </div>
   )
 }
